3401a57af480fab284a7ecaf51e32fafb23579a1
adeshbagale <adeshbagale123@gmail.com>
2023-03-17 15:45:05 +0530

    updated

--- output.tf
+++ output.tf
@@ -1,1 +1,6 @@
-
+output "ec2_public_ip_address" {
+  value = aws_instance.instance2.public_ip
+}
+output "ec2_private_ip_address" {
+  value = aws_instance.instance2.private_ip
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,24 @@
+variable "key_name" {
+  type = string
+  default = "my_key"
+}
 
+variable "key_path" {
+  type = string
+  default = "./keys/my_key.pem"
+
+}
+
+variable "ami_id" {
+   type = string
+}
+
+variable "instance_type" {
+   type = string
+}
+
+variable "sg_grp" {
+   type = string
+   default = "my_security_group"
+
+}
